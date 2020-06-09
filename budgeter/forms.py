b9565c7f4fe4f2cda48c5de3f602878c7e43b3fa
William Baxter <wbaxter3@gmail.com>
2020-06-08 23:01:05 -0400

    small fixes

--- budgeter/forms.py
+++ budgeter/forms.py
@@ -3,8 +3,8 @@ from django import forms
 
 class BudgetForm(forms.Form):
 
-    budget_name = forms.CharField(label='budget name', max_length=100)
-    max_amount = forms.FloatField(label='max amount', min_value=0, widget=forms.NumberInput(attrs={'step':"0.01"}))
+    budget_name = forms.CharField(label='Budget name', max_length=100)
+    max_amount = forms.FloatField(label='Max amount', min_value=0, widget=forms.NumberInput(attrs={'step':"0.01"}))
 
 
 class AddToAmount(forms.Form):
@@ -13,10 +13,10 @@ class AddToAmount(forms.Form):
     budget_id = forms.IntegerField(widget=forms.HiddenInput())
 
 class EditBudget(forms.Form):
-    budget_name = forms.CharField(label='budget name', max_length=100)
-    max_amount = forms.FloatField(label='max amount', min_value=0,widget=forms.NumberInput(attrs={'step':"0.01"}))
-    current_amount = forms.FloatField(label='current amount', min_value=0,widget=forms.NumberInput(attrs={'step':"0.01"}))
+    budget_name = forms.CharField(label='Budget name', max_length=100)
+    max_amount = forms.FloatField(label='Max amount', min_value=0,widget=forms.NumberInput(attrs={'step':"0.01"}))
+    current_amount = forms.FloatField(label='Current amount', min_value=0,widget=forms.NumberInput(attrs={'step':"0.01"}))
     budget_id = forms.IntegerField(widget=forms.HiddenInput())
 
 class EditMonthlyIncome(forms.Form):
-    monthly_income = forms.FloatField(label='monthly income', min_value=0,widget=forms.NumberInput(attrs={'step':"0.01"}))
+    monthly_income = forms.FloatField(label='Monthly income', min_value=0,widget=forms.NumberInput(attrs={'step':"0.01"}))
